80a44fb4fd2b1d4ec466e08ef92e3d583e552db5
jaysmkar <jayeshmandavkar5042@gmail.com>
2024-02-22 14:25:50 +0530

    adding some more points to the second_commit.py file

--- second commit.py
+++ second commit.py
@@ -1,1 +1,3 @@
-print("this is the second commit for test_branch")
+print("This is the second commit for test_branch")
+
+print("doing some changes in the second commit file so that it will be added to the git finally.")
